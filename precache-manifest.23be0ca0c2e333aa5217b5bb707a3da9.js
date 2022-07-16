37ec9aa804a1285519928fd4aceb8eef1c550977
1nhale <gurinder18@outlook.com>
2022-07-16 15:14:57 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.23be0ca0c2e333aa5217b5bb707a3da9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58766b0e4ca3aa107eafc998a440f91e",
+    "url": "/React-TODO-App/index.html"
+  },
+  {
+    "revision": "098bdc39a3c2513b6752",
+    "url": "/React-TODO-App/static/css/main.ee5d6995.chunk.css"
+  },
+  {
+    "revision": "58e9e32489f205a49016",
+    "url": "/React-TODO-App/static/js/2.a6507c3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-TODO-App/static/js/2.a6507c3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098bdc39a3c2513b6752",
+    "url": "/React-TODO-App/static/js/main.23cb95bd.chunk.js"
+  },
+  {
+    "revision": "b06edd23784cbd16b794",
+    "url": "/React-TODO-App/static/js/runtime-main.e4bf0842.js"
+  }
+]);
